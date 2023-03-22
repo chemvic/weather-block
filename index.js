392f746562e9442d81b3d70db08e9fb5487cfcb2
chemvic <chemvic79@gmail.com>
2023-03-22 23:36:29 +0200

    округление температуры

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function showLocation(position) {
   getWeather(lat, lon); // вызов функции для получения погоды
 }
 
-function getWeather(lat, lon) {
+function getWeather(lat=50.4333, lon=30.5167) {
     const API_KEY = "2e7139c6a4f80c6748f001736e0d42cb";
     const BASE_URL="https://api.openweathermap.org/data/2.5/weather"
   const url = `${BASE_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
@@ -28,18 +28,14 @@ function getWeather(lat, lon) {
     .then(response => response.json())
     .then(data => { drawWeather(data); })
         .catch(error => console.log(error));
-    
+   
 }
-
-// Для погоды на неделю
-// http://api.openweathermap.org/data/2.5/forecast/
-// daily ? q = London & cnt=7 & appid={ YOUR_API_KEY }
 
 getLocation();
 
 function drawWeather(data) {
-     
-    const markup = `<div class="info"><span class="degree">${data.main.temp}°</span><div class="navigation"><span class="description">|${data.weather[0].description}</span>
+      console.log(data);
+    const markup = `<div class="info"><span class="degree">${Math.round(data.main.temp)}°</span><div class="navigation"><span class="description">|${data.weather[0].description}</span>
 <span class="location">${data.name}</span></div></div>
 <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather icon" class="weatherImg" width="365px" height="356px">
 <p class="dayOfWeek">${dayOfWeek}</p>
@@ -47,3 +43,9 @@ function drawWeather(data) {
 {/* <a class="weekWeather" href="https://openweathermap.org/city/${data.id}">weather for week</a> */}
     weatherEl.innerHTML=markup;  
 }
+
+
+
+// Для погоды на неделю
+// http://api.openweathermap.org/data/2.5/forecast/
+// daily ? q = London & cnt=7 & appid={ YOUR_API_KEY }
